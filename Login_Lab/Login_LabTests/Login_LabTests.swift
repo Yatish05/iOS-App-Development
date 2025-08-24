5c2c6d10f70fae652eddc465bb58114bab9ec851
Yatish05 <yatishbansal05@gmail.com>
2025-08-24 21:28:27 +0530

    updated

---
+++ Login_Lab/Login_LabTests/Login_LabTests.swift
@@ -0,0 +1,11 @@
+
+import Testing
+@testable import Login_Lab
+
+struct Login_LabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
